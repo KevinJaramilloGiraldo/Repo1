3ee86c36ec987a789a8e3c3d204e81bdd5d78bc9
KevinJaramilloGiraldo <91032691+KevinJaramilloGiraldo@users.noreply.github.com>
2024-08-22 20:54:59 -0500

    Mi primera versión del archivo
    Mi primera versión del archivo xd

---
+++ factoriales.py
@@ -0,0 +1,7 @@
+#Hacer factoriales
+num=int(input("Ingrese número para calcular el factorial: ")) #Declara la variable número, le asigna un valor de tipo entero, y con el input imprime letrero y pide dato
+fact = 1 #Se declara la variable que luego convierten en funcion, si pongo igual 0, por la forma en que más adelante se crea la función todo tambien se vuelve 0
+for i in range(num+1):#se incrementa el num en una unidad porque python toma el range o rango como indice, por lo cual inicia contando la posición 0 como una unidad
+    if i !=0:#condición para que no todo se vuelva 0
+        fact = fact * i#vuelvo mi variable una función 
+print (fact)#imprimo el resultado
